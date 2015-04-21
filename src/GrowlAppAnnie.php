2c3473a8a60272b024898c097ca18bd2e27de5fe
Evgeniy Spinov <evgeniy@growlmedia.com>
2015-04-21 06:06:01 +0400

    *[API]: Updated version 1.0 -> 1.2. Some functions remains on 1.1 or 1.0 due to non-existance for 1.2

--- src/GrowlAppAnnie.php
+++ src/GrowlAppAnnie.php
@@ -23,63 +23,66 @@ class GrowlAppAnnie
 
     /**
      * Retrieve the all account connections, available in an App Annie account
-     * @link http://support.appannie.com/entries/23224068-1-Account-Connections-List
-     * @param array $params 
+     * @link http://support.appannie.com/hc/en-us/articles/204208994-1-Account-Connections-List
+     * @param array $params
      * @return array
      */
     public function getAccounts($params = array())
     {
-        return $this->sendRequest('get', $this->getUrl('/v1/accounts', $params));
+        return $this->sendRequest('get', $this->getUrl('/v1.2/accounts', $params));
     }
 
     /**
      * Retrieve a single account connection’s sales information
-     * 
-     * @link http://support.appannie.com/entries/23201091-1-Account-Connection-Sales
-     * @param string $accountId 
-     * @param array $params 
+     *
+     * @link http://support.appannie.com/hc/en-us/articles/204208954-1-Account-Connection-Sales
+     * @param string $accountId
+     * @param array $params
      * @return array
      */
     public function getAccountSales($accountId, $params = array())
     {
-        return $this->sendRequest('get', $this->getUrl("/v1/accounts/$accountId/sales", $params));
+        return $this->sendRequest('get', $this->getUrl("/v1.2/accounts/$accountId/sales", $params));
     }
 
     /**
      * Retrieve the sales data for a single app.
-     * 
-     * @link http://support.appannie.com/entries/23215097-3-App-Sales
-     * @param string $accountId 
-     * @param string $appId 
-     * @param array $params 
+     *
+     * @link http://support.appannie.com/hc/en-us/articles/204208914-3-Product-Sales-
+     * @param string $accountId
+     * @param string $appId
+     * @param array $params
      * @return array
      */
-    public function getAccountAppSales($accountId, $appId, $params = array())
+    public function getAccountAppSales($accountId, $productId, $params = array())
     {
-        return $this->sendRequest('get', $this->getUrl("/v1/accounts/$accountId/apps/$appId/sales", $params));
+        return $this->sendRequest('get', $this->getUrl("/v1.2/accounts/$accountId/products/$productId/sales", $params));
     }
 
     /**
      * Retrieve app list of one analytics Account Connection
-     * 
-     * @link http://support.appannie.com/entries/23215137-2-Account-Connection-App-List
-     * @param type $accountId 
-     * @param type $params 
+     *
+     * @link http://support.appannie.com/hc/en-us/articles/204208964-2-Account-Connection-Product-List
+     * @param type $accountId
+     * @param type $params
      * @return type
      */
     public function getAccountApps($accountId, $params = array())
     {
-        return $this->sendRequest('get', $this->getUrl("/v1/accounts/$accountId/apps", $params));
+        return $this->sendRequest('get', $this->getUrl("/v1.2/accounts/$accountId/products", $params));
     }
 
     /**
      * Retrieve an app’s detailed information.
-     * 
+     *
      * @link http://support.appannie.com/entries/23669989-2-App-Details
-     * @param string $vertical apps
+     * @param string $vertical apps | books
      * @param string $market ios | mac |google-play
-     * @param type $appId Which app results should be retrieved. For iOS and MAC, it's application id that defined by apple, Google Play, it's application class name. 
+     * @param type $appId Which app results should be retrieved. For iOS and MAC, it's application id that defined by apple, Google Play, it's application class name.
      * @return array
+     *
+     * Version 1.1 here as per recommendation from site.
+     *
      */
     public function getAppDetails($vertical, $market, $appId)
     {
@@ -88,31 +91,35 @@ class GrowlAppAnnie
 
     /**
      * List of countries
+     * @link http://support.appannie.com/hc/en-us/articles/204209024-1-Country-List-
      * @return array
      */
     public function getCountries()
     {
-        return $this->sendRequest('get', $this->getUrl("/v1.1/meta/countries"));
+        return $this->sendRequest('get', $this->getUrl("/v1.2/meta/countries"));
     }
 
     /**
      * Category list by market
-     * 
+     *
      * @link http://support.appannie.com/entries/23744774-2-Category-List-
-     * @param type $vertical apps
+     * @param type $vertical apps | books
      * @param type $market ios | mac |google-play
      * @return type
      */
     public function getCategories($vertical, $market)
     {
-        return $this->sendRequest('get', $this->getUrl("/v1.1/meta/$vertical/$market/categories"));
+        return $this->sendRequest('get', $this->getUrl("/v1.2/meta/$vertical/$market/categories"));
     }
 
     /**
      * List of platforms
-     * 
+     *
      * @link http://support.appannie.com/entries/21667604-3-Platform-List
      * @return array
+     *
+     * No replacement for this API in 1.2v
+     *
      */
     public function getPlatforms()
     {
@@ -121,19 +128,20 @@ class GrowlAppAnnie
 
     /**
      * Retrieve the In App Purchase list of one app
-     * @param string $accountId 
-     * @param string $appId 
-     * @param array $params 
+     * @link http://support.appannie.com/hc/en-us/articles/204208984-3-IAP-List
+     * @param string $accountId
+     * @param string $appId
+     * @param array $params
      * @return array
      */
-    public function getAccountAppIAPs($accountId, $appId, $params = array())
+    public function getAccountAppIAPs($accountId, $productId, $params = array())
     {
-        return $this->sendRequest('get', $this->getUrl("/v1/accounts/$accountId/apps/$appId/iaps"));
+        return $this->sendRequest('get', $this->getUrl("/v1.2/accounts/$accountId/products/$productId/iaps"));
     }
 
     /**
      * Build query string
-     * @param array $params 
+     * @param array $params
      * @return string
      */
     protected function getParams(array $params)
@@ -148,8 +156,8 @@ class GrowlAppAnnie
 
     /**
      * Get url for service with method and params
-     * @param string $method 
-     * @param array $params 
+     * @param string $method
+     * @param array $params
      * @return string
      */
     protected function getUrl($method, $params = array())
@@ -176,9 +184,9 @@ class GrowlAppAnnie
 
     /**
      * Send the request using curl
-     * @param string $method 
-     * @param string $url 
-     * @param array $postParams 
+     * @param string $method
+     * @param string $url
+     * @param array $postParams
      * @return array
      */
     protected function sendRequest($method, $url, $postParams = array())
@@ -210,9 +218,9 @@ class GrowlAppAnnie
 
     /**
      * Process response from web service
-     * 
+     *
      * @throws GrowlAppAnnieException
-     * @param string $response 
+     * @param string $response
      * @return array
      */
     protected function processResponse($response)
